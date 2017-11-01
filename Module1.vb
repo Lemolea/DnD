9b4391135c51fbdd6baaf23588c97607afc8711f
Felix9487 <33281535+Felix9487@users.noreply.github.com>
2017-11-01 15:35:46 +0000

    Add files via upload

--- Module1.vb
+++ Module1.vb
@@ -1,13 +1,15 @@
-﻿Module Module1
+﻿Imports System.Threading
 
-    Structure player
+Module Module1
+
+    Structure Player
         Public name As String
         Public lifeTotal As Integer
         Public gold As Integer
-        Public weapon As weapon
+        'Public weapon As Weapon
     End Structure
 
-    Structure weapon
+    Structure Weapon
         Public name As String
         Public damage As Integer
     End Structure
@@ -20,31 +22,27 @@
 
     Structure trap
         Public name As String
-        Public correct_option As String
+        Public background As String
+        Public correctOption As String
+        Public wrongOption1 As String
+        Public wrongOption2 As String
     End Structure
-    Sub Main()
-        Dim thePlayer As player
-        Dim theWeapon As weapon
 
-        thePlayer.lifeTotal = 20
-        thePlayer.gold = 0
-        theWeapon.name = "Fist"
-        theWeapon.damage = 1
+    Function diceRoll(ByRef upperBound As Integer)
+        Dim roll As New Random
+        roll = New Random
+        Return roll.Next(1, upperBound)
+    End Function
 
-        Console.Write("So, what would you like to be called? ")
-        thePlayer.name = Console.ReadLine()
-        Console.WriteLine("Today you will attack you foes with your " & theWeapon.name)
-
-        Console.Read()
-
-    End Sub
-
-    Function Weapons(ByRef theWeapon As weapon)
-        Select Case d20
+    Function Weapons(ByRef theWeapon As Weapon)
+        Select Case diceRoll(2)
             Case 1
+                theWeapon.name = "Fist"
+                theWeapon.damage = 1
+            Case 2
                 theWeapon.name = "Sword"
                 theWeapon.damage = 5
-            Case 2
+            Case 3
                 theWeapon.name = "Slightly Bigger Sword TM"
                 theWeapon.damage = 10
         End Select
@@ -53,7 +51,7 @@
     End Function
 
     Function livingEntity(ByRef mob As living_entity)
-        Select Case d20
+        Select Case diceRoll(2)
             Case 1
 
             Case 2
@@ -62,30 +60,98 @@
         Return mob
     End Function
 
-    Function encounter(ByRef trap As trap)
-        Select Case d20
-            Case 1
+    Function Encounter() As trap
+        Dim trap As trap
+        Dim whichtrap As Integer
 
+        whichtrap = diceRoll(1)
+
+        Select Case whichtrap
+            Case 1
+                trap.name = "Bottomless pit"
+                trap.background = "You see a bottomless pit open up infront of you. What do you do?"
+                trap.correctOption = "1: Walk around it"
+                trap.wrongOption1 = "2: You jump over the pit"
+                trap.wrongOption2 = "3: You spin 180° and run away"
             Case 2
+            Case 3
+            Case 4
 
         End Select
+
         Return trap
     End Function
 
-    Sub Level_1()
+    Sub Main()
+        Dim thePlayer As Player
+        Dim theWeapon As Weapon
 
+        thePlayer.lifeTotal = 20
+        thePlayer.gold = diceRoll(10)
+        theWeapon.name = "Fist"
+        theWeapon.damage = 1
+
+        Console.WriteLine("")
+
+        Console.WriteLine("So, what would you like to be called? ")
+        thePlayer.name = Console.ReadLine()
+        Console.WriteLine("Today you will attack your foes with your " & theWeapon.name)
+
+        Level_1(thePlayer, theWeapon)
 
     End Sub
-    Sub Level_2()
 
+    Sub Level_1(ByRef thePlayer As Player, ByRef theWeapon As Weapon)
+        Dim trap As trap
+        Dim answer As String
+        Dim ans As Boolean = False
+        trap = Encounter()
+
+        Console.WriteLine("You walk into a cave")
+        Console.WriteLine(trap.background)
+        Console.WriteLine(trap.correctOption)
+        Console.WriteLine(trap.wrongOption1)
+        Console.WriteLine(trap.wrongOption2)
+
+
+        While ans = False
+
+            answer = Console.ReadLine()
+
+            If answer = "1" Then
+                Console.WriteLine("1")
+                ans = True
+            ElseIf answer = "2" Then
+                Console.WriteLine("2")
+                ans = True
+            ElseIf answer = "3" Then
+                Console.WriteLine("3")
+                ans = True
+            Else
+                Console.WriteLine("Please enter a value between 1 and 3")
+
+            End If
+
+
+        End While
+
+
+
+        Thread.Sleep(10000)
+    End Sub
+    Sub Level_2()
+        Dim thePlayer As Player
+        thePlayer.lifeTotal = 23
 
     End Sub
     Sub Level_3()
-
+        Dim thePlayer As Player
+        thePlayer.lifeTotal = 26
 
     End Sub
     Sub Level_4()
-
+        Dim thePlayer As Player
+        thePlayer.lifeTotal = 30
 
     End Sub
 
