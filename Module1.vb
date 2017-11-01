31a161dd3bbad78f72fdcb93b0bcfa1499105699
Felix9487 <33281535+Felix9487@users.noreply.github.com>
2017-11-01 10:45:36 +0000

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim playerName As String
+        Dim playerClass As String
+
+
+        Console.Write("So, what would you like to be called?")
+        playerName = Console.ReadLine()
+        Console.Write("And what about your class?")
+        playerClass = Console.ReadLine()
+
+        Console.Read()
+    End Sub
+
+End Module
